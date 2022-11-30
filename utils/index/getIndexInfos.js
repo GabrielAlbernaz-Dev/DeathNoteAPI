43c3cf22b56646ba6e3e2890043d8be38abc39ae
Gabriel Albernaz <gabrielalbernaz22@gmail.com>
2022-11-29 22:52:26 -0300

    swagger release

--- utils/index/getIndexInfos.js
+++ utils/index/getIndexInfos.js
@@ -4,9 +4,11 @@ function getIndexInfos() {
             login:'localhost:3000/auth/user',
             register: 'localhost:3000/auth/register'
         },
+        docs:"http://localhost:3000/docs",
         anime:"localhost:3000/anime",
         characters:"localhost:3000/characters",
-        organizations:"localhost:3000/organizations"
+        organizations:"localhost:3000/organizations",
+        places:"localhost:3000/places"
     }
 }
 
